c742e2c6f05b6a9f50351621c477ebbcf7155aaa
Takeru Kakuta <kakuta19960915@gmail.com>
2024-10-04 20:36:00 +0900

    機能実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
+import { doc, collection, getDocs, onSnapshot } from "firebase/firestore";
 import "./App.css";
+import db from "./firebase";
 
 function App() {
-  return <div className="App">テスト</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // データベースから取得する
+    const postData = collection(db, "posts");
+    getDocs(postData).then((snapShot) => {
+      // console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
+      setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })));
+    });
+
+    // リアルタイムで取得
+    onSnapshot(postData, (post) => {
+      setPosts(post.docs.map((doc) => ({ ...doc.data() })));
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        {posts.map((post) => (
+          <div key={post.title}>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDWJfk-eBkIB8vFRQ773w3XUsyIQ2tcJC4",
@@ -10,3 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;

84ba90772b9117386aebd86fe31839ee9ebe79ca
Takeru Kakuta <kakuta19960915@gmail.com>
2024-10-04 19:42:07 +0900

    firebase初期化

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWJfk-eBkIB8vFRQ773w3XUsyIQ2tcJC4",
+  authDomain: "fir-react-tutorial-ae964.firebaseapp.com",
+  projectId: "fir-react-tutorial-ae964",
+  storageBucket: "fir-react-tutorial-ae964.appspot.com",
+  messagingSenderId: "665293135625",
+  appId: "1:665293135625:web:9b3267c86f225cf443fdd8",
+};
+
+const app = initializeApp(firebaseConfig);
